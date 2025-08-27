bea8372029eb59af3f3b1a6c6671537dba735dc0
Caarlos3 <crt.981995@gmail.com>
2025-08-27 17:47:49 +0200

    añadido grafico

---
+++ front/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Grafic from './components/Grafic';
+
+function Home() {
+  return (
+    <div>
+     <Grafic />
+    </div>
+  );
+}
+
+export default Home;
---
+++ front/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+export default function Navbar() {
+    return (   
+
+      <nav style={{
+        height: '40px',
+        width: '100%',
+        backgroundColor: 'black',
+        color: 'white',
+        
+      }}>
+        <nav style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+            
+            
+        }}>
+            Let's Fit App
+        </nav>
+      </nav>
+
+
+
+      );
+};
